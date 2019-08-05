66f294a79f052419097a7a5987d47c7aacf9c03d
Alexander Pehm <alexander.pehm@paysafe.com>
2019-08-04 18:04:14 -0700

    Iteration 01 - Web components intro

---
+++ src/rfk-app.js
@@ -0,0 +1,17 @@
+class RfkApp extends HTMLElement {
+
+    constructor(){
+        // Always call the parent constructor first
+        super();
+        // Attach a new shadow root
+        const root = this.attachShadow({mode: "open"});
+        // Clone the template
+        const template = document.getElementById("app-greeting");
+        const templateContent = template.content;
+        // append the template 
+        root.appendChild(
+            templateContent.cloneNode(true)
+          );
+    }
+}
+window.customElements.define('rfk-app', RfkApp);
